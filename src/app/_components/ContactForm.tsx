d40593ef1046a9072cbb624fe394a405edd1a075
mihonishiwaki <miho.n.6371@gmail.com>
2025-04-16 23:57:50 +0900

    input type変更

--- src/app/_components/ContactForm.tsx
+++ src/app/_components/ContactForm.tsx
@@ -15,15 +15,17 @@ export default function ContactForm() {
               type="text"
               id="name"
               name="name"
+              required
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 forcus:ring-2 focus:ring-indigo-200 outline-none py-1 px-3 leading-8"
             />
             <label htmlFor="email" className="text-sm mt-2 mb-1">
               メールアドレス
             </label>
             <input
-              type="text"
+              type="email"
               id="email"
               name="email"
+              required
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 forcus:ring-2 focus:ring-indigo-200 outline-none py-1 px-3 leading-8"
             />
             <button className="text-white bg-indigo-500 mt-5 py-2 px-6 hover:bg-indigo-600 rounded text-lg">
